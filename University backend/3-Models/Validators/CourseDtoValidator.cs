1c6597d841dffe780d999217acc2c9ec4752d82d
lakekan <itay.cohen2907@gmail.com>
2025-03-06 12:49:56 +0200

    Validators

---
+++ University backend/3-Models/Validators/CourseDtoValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace University_backend;
+
+public class CourseDtoValidator : AbstractValidator<CourseDto>
+{
+    public CourseDtoValidator()
+    {
+        RuleFor(course => course.Title)
+            .NotEmpty().WithMessage("Missing title.")
+            .MinimumLength(5).WithMessage("Title must be minimum 5 characters.")
+            .MaximumLength(100).WithMessage("Title can't exceed 100 characters.");
+
+        RuleFor(course => course.Description)
+            .NotEmpty().WithMessage("Missing description.")
+            .MinimumLength(10).WithMessage("Description must be minimum 10 characters.")
+            .MaximumLength(500).WithMessage("Description can't exceed 500 characters.");
+
+        RuleFor(course => course.CreatedAt)
+            .NotEmpty().WithMessage("Creation date is required.");
+    }
+}
